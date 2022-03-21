9dfaf69a7399103ad40a4901d5ab4c9d1d17e16c
Felipe Huffner <felipeqh.1991@gmail.com>
2022-03-21 14:01:36 -0400

    Fixing Header Test

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -22,7 +22,7 @@ const Header = (): JSX.Element => {
         <div>
           <strong>Meu carrinho</strong>
           <span data-testid="cart-size">
-            { `${cartSize} === 1` ? `${cartSize} item` : `${cartSize} itens`} 
+            { cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`} 
           </span>
         </div>
         <MdShoppingBasket size={36} color="#FFF" />
